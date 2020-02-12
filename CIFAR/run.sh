68a4f9a077670997b2a96a7b0471313992eee148
Boyi Li <32026822+Boyiliee@users.noreply.github.com>
2020-02-11 19:10:12 -0500

    Create run.sh

---
+++ CIFAR/run.sh
@@ -0,0 +1,17 @@
+DATA=cifar100 #cifar10
+JOB=pyramidnet_moex
+epoch=300
+lam=0.5
+prob=0.25
+
+python train_pono.py \
+    --net_type pyramidnet_moex \
+    --dataset ${DATA} \
+    --depth 200 \
+    --alpha 240 \
+    --batch_size 64 \
+    --lr 0.25 \
+    --expname ${JOB} \
+    --epochs ${epoch} \
+    --beta 1.0 \
+    --lam ${lam} --pono_prob ${prob}
